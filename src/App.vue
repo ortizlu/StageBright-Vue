b91c27652b7cd10aa8f67069549392d3c75abb25
Luis Ortiz <ortizlu@icloud.com>
2018-10-29 15:49:46 -0400

    started working on react example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Splashscreen />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Splashscreen from './components/Splashscreen.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Splashscreen
   }
 }
 </script>
---
+++ src/components/Splashscreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Splashscreen">
+    {{title}}
+    <p>{{user.firstName}}</p>
+    <p v-text="user.firstName"></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Splashscreen',
+    data() {
+      return {
+        title: '<h1>Hello world</h1>',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
